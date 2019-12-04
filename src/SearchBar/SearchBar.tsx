f06b4fb706b5663ec5ec43b2573e007de823dc54
Matt Pitcher <matt.e.pitcher@gmail.com>
2019-12-03 23:28:58 -0700

    mobile menu

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -8,7 +8,7 @@ const Header = styled.header`
   width: 100vw;
   background-color: #ffde00;
   height: 70px;
-  left: -8px;
+  left: 0;
   display: flex;
   align-items: center;
 `;
@@ -22,21 +22,29 @@ const Logo = styled.div`
 `;
 
 const LogoImage = styled.img`
-  height: 50px;
-  width: 50px;
+  height: 60px;
+  width: 60px;
 `;
 
 const LogoText = styled.span`
   font-family: 'Proza sans-serif';
   font-weight: bold;
   font-size: 40px;
-  padding: 0 15px;
+  padding: 0 5px;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 const FilterContainer = styled.div`
   display: flex;
   position: absolute;
   right: 25px;
+
+  @media (max-width: 1300px) {
+    display: none;
+  }
 `;
 
 const Filter = styled.div`
@@ -50,6 +58,47 @@ const FilterLabel = styled.label`
   padding: 5px;
 `;
 
+const MobileFilterToggle = styled.img`
+  display: none;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  height: 50px;
+  width: 50px;
+  pointer: cursor;
+
+  @media (max-width: 1300px) {
+    display: block;
+  }
+`;
+
+interface MobileFilter {
+  display: string;
+  left: string;
+}
+
+const MobileFilterContainer = styled.div<MobileFilter>`
+  display: block;
+  transition: left 1s;
+  left: ${({ left }) => left};
+  position: absolute;
+  top: 70px;
+  width: 100%;
+  height: calc(100vh - 70px);
+  background: lightgray;
+`;
+
+const MobileButtonContainer = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+`;
+
+const MobileButton = styled.button`
+  width: 100%;
+  height: 40px;
+`;
+
 interface SearchBarProps {
   pokemons: Pokemon[];
   onUpdatePokemons: (pokemons: Pokemon[]) => void;
@@ -60,16 +109,19 @@ export const SearchBar: React.FC<SearchBarProps> = ({ pokemons, onUpdatePokemons
   const [sortBy, setSortBy] = React.useState<string>('id');
   const [order, setOrder] = React.useState<number>(1); // 1 for asc, -1 for desc
   const [currPokemons, setCurrPokemons] = React.useState<Pokemon[]>(Array.from(pokemons));
+  const [mobileFilter, setMobileFilter] = React.useState<MobileFilter>({ display: 'none', left: '100%' });
 
-  const handleSearch = () => {
+  const handleSearch = (): Promise<Pokemon[]> => {
     const filteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().indexOf(name.toLowerCase()) !== -1);
     setCurrPokemons(filteredPokemons);
     onUpdatePokemons(filteredPokemons);
+    return new Promise<Pokemon[]>(resolve => {
+      return resolve(filteredPokemons);
+    });
   };
 
-  const handleSort = () => {
-    console.log(sortBy);
-    const sortedPokemon = Array.from(currPokemons.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 * order : -1 * order)));
+  const handleSort = (pokemonsToSort: Pokemon[]) => {
+    const sortedPokemon = Array.from(pokemonsToSort.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 * order : -1 * order)));
     onUpdatePokemons(sortedPokemon);
   };
 
@@ -80,37 +132,65 @@ export const SearchBar: React.FC<SearchBarProps> = ({ pokemons, onUpdatePokemons
     setOrder(1);
   };
 
+  const handleApplyAll = () => {
+    handleSearch().then(filteredPokemons => handleSort(filteredPokemons));
+    closeMobileMenu();
+  };
+
+  const closeMobileMenu = () => {
+    setMobileFilter({ display: 'none', left: '100%' });
+  };
+
+  const Filters = (
+    <React.Fragment>
+      <Filter>
+        <FilterLabel htmlFor="pokemon_name">Name</FilterLabel>
+        <input type="text" id="pokemon_name" value={name} onChange={e => setName(e.currentTarget.value)} />
+        <button onClick={handleSearch}>Search</button>
+      </Filter>
+      <Filter>
+        <FilterLabel htmlFor="sort_by">Sort By</FilterLabel>
+        <select id="sort_by" value={sortBy} onChange={e => setSortBy(e.currentTarget.value)}>
+          <option value="id">ID</option>
+          <option value="name">Name</option>
+          <option value="maxHP">Hit Points</option>
+          <option value="maxCP">Combat Points</option>
+        </select>
+        <FilterLabel htmlFor="order">Order</FilterLabel>
+        <select id="order" value={order} onChange={e => setOrder(parseInt(e.currentTarget.value, 10))}>
+          <option value={1}>Ascending</option>
+          <option value={-1}>Descending</option>
+        </select>
+        <button onClick={() => handleSort(currPokemons)}>Sort</button>
+      </Filter>
+    </React.Fragment>
+  );
+
   return (
     <Header>
       <Logo>
-        <LogoImage src="https://webstockreview.net/images/pokeball-clipart-transparent-background-3.png" />
+        <LogoImage src="https://icon-library.net/images/pokemon-pokeball-icon/pokemon-pokeball-icon-6.jpg" />
         <LogoText>Ketchum's Collection</LogoText>
       </Logo>
       <FilterContainer>
-        <Filter>
-          <FilterLabel htmlFor="pokemon_name">Name</FilterLabel>
-          <input type="text" id="pokemon_name" value={name} onChange={e => setName(e.currentTarget.value)} />
-          <button onClick={handleSearch}>Search</button>
-        </Filter>
-        <Filter>
-          <FilterLabel htmlFor="sort_by">Sort By</FilterLabel>
-          <select id="sort_by" value={sortBy} onChange={e => setSortBy(e.currentTarget.value)}>
-            <option value="id">ID</option>
-            <option value="name">Name</option>
-            <option value="maxHP">Hit Points</option>
-            <option value="maxCP">Combat Points</option>
-          </select>
-          <FilterLabel htmlFor="order">Order</FilterLabel>
-          <select id="order" value={order} onChange={e => setOrder(parseInt(e.currentTarget.value, 10))}>
-            <option value={1}>Ascending</option>
-            <option value={-1}>Descending</option>
-          </select>
-          <button onClick={handleSort}>Sort</button>
-        </Filter>
+        {Filters}
         <Filter>
           <button onClick={resetPokemon}>Reset</button>
         </Filter>
       </FilterContainer>
+      <MobileFilterToggle
+        role="button"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"
+        onClick={() => setMobileFilter({ display: 'flex', left: '0' })}
+      />
+      <MobileFilterContainer {...mobileFilter}>
+        {Filters}
+        <MobileButtonContainer>
+          <MobileButton onClick={handleApplyAll}>Apply All</MobileButton>
+          <MobileButton onClick={resetPokemon}>Reset</MobileButton>
+          <MobileButton onClick={closeMobileMenu}>Close Menu</MobileButton>
+        </MobileButtonContainer>
+      </MobileFilterContainer>
     </Header>
   );
-}
+};
